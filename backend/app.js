d846da5eff00ecddffa34a9fde04b1d98851e48e
Neeraj chauhan <nc1676639@gmail.com>
2026-03-30 11:27:45 +0530

    link add

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://interview-ai-resume.onrender.com",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   }),
